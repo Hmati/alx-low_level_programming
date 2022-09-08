81a36f95ecd35622dd4e09e02caf4c4fb2e27412
Hmati <mosesmati93@gmail.com>
2022-09-08 14:45:02 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * Starting point 
+ *
+ * Return 0  if a success
+ *
+ */
+int main(void)
+
+{
+   put ("\"Programming is like building a multilingual puzzle");
+   Return  (0);
+}
