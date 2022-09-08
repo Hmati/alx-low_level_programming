a18d1c26d775e787144f67495cc6ec6a70a49e09
Hmati <mosesmati93@gmail.com>
2022-09-08 15:34:51 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+* main - print function in printf function 
+* Description : using the main function 
+* Return :0
+*/
+
+int main(void)
+{
+
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+
+        return(0);
+}
