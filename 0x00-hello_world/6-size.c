e5be7623b50fb6d0ecc6e8895a0921568e0f8acd
Hmati <mosesmati93@gmail.com>
2022-09-09 02:19:40 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,4 @@
 #include<stdio.h>
-
-
 /**
  * main-print the sring in the main function 
  *
@@ -14,9 +12,9 @@ int main(void)
         long int li;
         long long lli;
 
-	printf("Size of a int : %ld  byte(s)\n", (unsigned long)sizeof(i));
-	printf("Size of a char :%ld byte(s)\n",(unsigned long)sizeof(a));
-        printf("Size of a float : %ld byte(s)\n", (unsigned long)sizeof(f));	
-        printf("Size of a long int : %ld byte(s)\n", (unsigned long)sizeof(li));
-        printf("Size of a Long long int : %ld  byte(s)\n", (unsigned long)sizeof(lli));
+	printf("Size of a int : %d  byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a char :%d byte(s)\n",(unsigned long)sizeof(a));
+        printf("Size of a float : %d byte(s)\n", (unsigned long)sizeof(f));	
+        printf("Size of a long int : %d byte(s)\n", (unsigned long)sizeof(li));
+        printf("Size of a Long long int : %d  byte(s)\n", (unsigned long)sizeof(lli));
 }       return(0);
