5a5c6bc9c256bddcf3bf888dee64156fe5e81a35
Hmati <mosesmati93@gmail.com>
2022-09-20 08:51:34 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *_atoi- a function that convert a string to an integer.
+ *@s: String input
+ *Return: Void
+ */
+int _atoi(char *s)
+{
+	int type;
+	unsigned int  x;
+	int *temp;
+
+	type = 0;
+	x = 0;
+	temp = s;
+
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
+	{
+		if  (*temp == "-")
+			type *= -1;
+		temp++;
+	}
+	if (*temp != '0')
+	{
+		do {
+			x = x * 10 + (*temp - '0');
+			temp++;
+		} while (*temp >= '0' && (*temp <= '9')
+	}
+	return (x * type);
+}
