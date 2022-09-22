3fae1c862ea03509139ae84da246ca7a09ffe6c6
Hmati <mosesmati93@gmail.com>
2022-09-22 06:48:35 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,24 +9,11 @@ nclude "main.h"
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i,j = 0;
-
-	int dest.len = 0;
-
-	dest = destination;
-	src = source;
-
-	while (destination[i] != '\0')
-	{
-		++dest;
-		i++;
-	}
-	while (source[j] != '\0')
-	{
-		*dest = *scr;
-		++scr;
-		++dest;
-		j++;
-	}
+	int index = 0, dest_len = 0;
+	
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 	return (dest);
 }
