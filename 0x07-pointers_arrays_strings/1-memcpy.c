35a87a82852708678f91ca4841ba2f0d481a7a8c
Hmati <mosesmati93@gmail.com>
2022-09-27 06:34:51 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,7 +10,7 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int index;
+	unsigned int index = 0;
 	unsigned char *destination = dest;
 	const unsigned char *source = src;
 
