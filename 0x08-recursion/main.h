2832eed9b8b91c5c48a01fd0754e8cbbb364aa38
Hmati <mosesmati93@gmail.com>
2022-09-28 14:12:59 -0700

    0-puts_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,7 +7,7 @@
  *Desc: Header file containing prototypes for all functions
  */
 
-int _putchar(char c);                
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int factorial(int n);
