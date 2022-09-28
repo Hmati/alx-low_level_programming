4fa7d44cf3a3dde44da01abfbc49c25d667e20bb
Hmati <mosesmati93@gmail.com>
2022-09-28 14:02:05 -0700

    0-puts_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,9 +1,12 @@
-#ifndef MAIN_H                                                                                                          #define MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
+
 /**
  *file:main.h
  *Auth: me 
  *Desc: Header file containing prototypes for all functions
  */
+
 int _putchar(char c);                
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -13,5 +16,6 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);                                                                                             
-int wildcmp(char *s1, char *s2);                                                                                       
+int wildcmp(char *s1, char *s2);
+
 #endif
