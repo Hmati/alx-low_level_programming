9b54436ad1a70bd3a210b9ef6e2a2269b92d5347
Hmati <mosesmati93@gmail.com>
2022-09-30 10:29:19 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,9 +11,9 @@ int main(int argc, char *argv)
 {
 	int x;
 
-	for (x = 0, x < argc, x++)
+	for (x = 0; x < argc; x++)
 	{
-		printf("%d\n", argv[i]);
+		printf("%d\n", argv[x]);
 	}
 	return (0);
 }
