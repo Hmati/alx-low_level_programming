cd6460ab74875f2ef47ca9ff8275280b4f8d08f6
Hmati <mosesmati93@gmail.com>
2022-10-07 15:54:09 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,10 @@
+#include<unistd.h>
+/**
+ * _putchar - writes character c to stdou
+ * @c: character to print 
+ * Return :returns 1 on success -1 on error 
+ */
+int _putchar(char c)
+{
+	return(write(1, &, 1))
+}
