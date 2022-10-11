8e07ada97ace0dcffe6bdd9b1666bb22b506180e
Hmati <mosesmati93@gmail.com>
2022-10-11 15:48:26 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -46,7 +46,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (name == NULL || owner == NULL)
 	{
 		return (NULL);
-	{
+	}
 		snoopie = malloc(sizeof(dog_t));
 		if (snoopie == NULL)
 		{
